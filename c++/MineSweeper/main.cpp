13ea839b07189bc7a30933aaa1a0131fb3517148
Kevin Amiranoff <kamiranoff@gmail.com>
2020-04-03 02:13:21 +0100

    add user interaction with board

--- c++/MineSweeper/main.cpp
+++ c++/MineSweeper/main.cpp
@@ -1,4 +1,6 @@
 
+// Current progress
+// https://youtu.be/b2SL7xZ1IaM?t=2391
 #include "procedural/MineSweeper.h"
 
 
--- c++/MineSweeper/procedural/MineSweeper.cpp
+++ c++/MineSweeper/procedural/MineSweeper.cpp
@@ -8,13 +8,21 @@
 #define BOMB  " * ";
 #define UNDISCOVERED_CELL "⬜";
 #define FREE_CELL "   ";
+#define UP 'w';
+#define DOWN 's';
+#define LEFT 'a';
+#define RIGHT 'd';
+#define USER_POSITION " + ";
+#define PRESS 'p';
 
 using namespace std;
 
 int lines = 9, columns = 9, bombs = 10;
-
+int is_game_finished = 0;
 string board[100][100];
 string user_board[100][100];
+int user_line = 0;
+int user_col = 0;
 
 int compute_neighbours(int line, int col);
 
@@ -83,14 +91,89 @@ void init_board() {
 void print_board() {
     for (int line = 0; line < lines; ++line) {
         for (int col = 0; col < columns; ++col) {
-            cout << board[line][col];
+            cout << user_board[line][col];
         }
         cout << '\n';
     }
 }
 
+void trigger_press(int line, int col) {
+    char press = PRESS;
+    string bomb = BOMB;
+    string p(1, press);
+    if (user_board[line][col] == p) {
+        return;
+    }
+
+    if (board[line][col] == bomb) {
+        is_game_finished = 1;
+        cout << "GAME OVER!\n";
+        for (int line = 0; line < lines; ++line) {
+            for (int col = 0; col < columns; ++col) {
+                if(board[line][col] == bomb) {
+                    cout << bomb;
+                }
+            }
+        }
+    } else {
+        user_board[line][col] = FREE_CELL;
+    }
+    cout << '\n';
+}
+
+void process_command(char command) {
+    char up = UP;
+    char down = DOWN;
+    char left = LEFT;
+    char right = RIGHT;
+    char press = PRESS;
+    string user_position = USER_POSITION;
+
+    int next_line = user_line;
+    int next_col = user_col;
+
+    if (command == up) {
+        --next_line;
+    } else if (command == down) {
+        ++next_line;
+    } else if (command == left) {
+        --next_col;
+    } else if (command == right) {
+        ++next_col;
+    }
+
+    if (!is_inside_field(next_line, next_col)) {
+        return;
+    }
+
+    if(user_board[user_line][user_col] == user_position) {
+        user_board[user_line][user_col] = UNDISCOVERED_CELL;
+    }
+    user_line = next_line;
+    user_col = next_col;
+    user_board[next_line][next_col] = USER_POSITION;
+
+    if (command == press) {
+        trigger_press(user_line, user_col);
+    }
+}
+
+void start_game() {
+    user_line = user_col = 0;
+    is_game_finished = 0;
+    init_board();
+    user_board[user_line][user_col] = USER_POSITION;
+
+}
+
 void start() {
     srand(time(nullptr));
-    init_board();
-    print_board();
+    start_game();
+    while (!is_game_finished) {
+        print_board();
+        char command;
+        cout << "Input command: ";
+        cin >> command;
+        process_command(command);
+    }
 }
